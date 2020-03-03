4c2eb433dae4c7189b6d54acf9010105a5037735
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-03-02 19:38:14 -0500

    Updates

---
+++ precache-manifest.f34f18fbdcbc45066d946448b600b48e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112044fbea1208693d5b070abd856916",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2e16d832a75a421b531",
+    "url": "/portfolio/static/css/main.358a8bea.chunk.css"
+  },
+  {
+    "revision": "d09d37a9843a83dc241a",
+    "url": "/portfolio/static/js/2.28499a05.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.28499a05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2e16d832a75a421b531",
+    "url": "/portfolio/static/js/main.fb7087ed.chunk.js"
+  },
+  {
+    "revision": "bbff5ecae590bc365cda",
+    "url": "/portfolio/static/js/runtime-main.e77b87bb.js"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/portfolio/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/portfolio/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "bd0d40f6cc30779bcfcb06e72c1aa0fd",
+    "url": "/portfolio/static/media/LogoMakr_2ezPuQ.bd0d40f6.png"
+  },
+  {
+    "revision": "e8f51b832fd8abe3b930285ec1e0fde5",
+    "url": "/portfolio/static/media/basketball-concordance.e8f51b83.png"
+  },
+  {
+    "revision": "3ef6361e5833acac21a237a399d150f7",
+    "url": "/portfolio/static/media/homescreen-screenshot.3ef6361e.png"
+  },
+  {
+    "revision": "dfc25cc79f297ec802e0b2944634b2ae",
+    "url": "/portfolio/static/media/innerCircle.dfc25cc7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.15db5503164d6c997be2a74f5b9951e2.js"
+  "/portfolio/precache-manifest.f34f18fbdcbc45066d946448b600b48e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
